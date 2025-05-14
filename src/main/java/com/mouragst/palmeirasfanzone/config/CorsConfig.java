1f6429eaa96954bc4822a72a005076f55786c015
gmourarpn <gustavo.moura@gruporpn.com.br>
2025-05-14 11:22:23 -0300

    feat: adicionado config cors

---
+++ src/main/java/com/mouragst/palmeirasfanzone/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.mouragst.palmeirasfanzone.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${FRONT_END_URL}")
+    private String frontEndUrl;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin(frontEndUrl);
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
